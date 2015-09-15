58d7bff267c414ffb0b2932224f0e98c92bc47c6
wendal <wendal1985@gmail.com>
2015-09-15 13:45:15 +0800

    add: 合并"短点"的功能

--- src/net/wendal/nutzbook/MainSetup.java
+++ src/net/wendal/nutzbook/MainSetup.java
@@ -107,7 +107,7 @@ public class MainSetup implements Setup {
 		
 		if (conf.getBoolean("socketio.enable", false))
 			ioc.get(SocketioService.class);
-		
+		Mvcs.disableFastClassInvoker = true;
 	}
 	
 	public void destroy(NutConfig conf) {
