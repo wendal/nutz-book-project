bf2f745e33e55191ae1008e01b0adcf703e3514b
wendal <wendal1985@gmail.com>
2016-01-07 17:26:50 +0800

    新增: 专题页

--- src/main/java/net/wendal/nutzbook/bean/yvr/TopicType.java
+++ src/main/java/net/wendal/nutzbook/bean/yvr/TopicType.java
@@ -2,7 +2,13 @@ package net.wendal.nutzbook.bean.yvr;
 
 public enum TopicType {
 
-	ask("问答"), news("新闻"), share("分享"), shortit("短点"), nb("灌水"), job("招聘");
+	ask("问答"), 
+	news("新闻"), 
+	share("分享"), 
+	shortit("短点"), 
+	nb("灌水"), 
+	job("招聘"),
+	special("专题");
 	
 	public String display;
 	
