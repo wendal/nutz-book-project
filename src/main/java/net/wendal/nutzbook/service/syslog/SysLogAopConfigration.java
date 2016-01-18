89ab69bd63b34174e286cf6735f75bf346abc83d
wendal <wendal1985@gmail.com>
2016-01-18 20:21:32 +0800

    fix import

--- src/main/java/net/wendal/nutzbook/service/syslog/SysLogAopConfigration.java
+++ src/main/java/net/wendal/nutzbook/service/syslog/SysLogAopConfigration.java
@@ -3,11 +3,11 @@ package net.wendal.nutzbook.service.syslog;
 import java.lang.reflect.Method;
 
 import org.nutz.aop.MethodInterceptor;
+import org.nutz.aop.SimpleAopMaker;
 import org.nutz.ioc.loader.annotation.Inject;
 import org.nutz.ioc.loader.annotation.IocBean;
 
 import net.wendal.nutzbook.annotation.SLog;
-import net.wendal.nutzbook.aop.SimpleAopMaker;
 
 @IocBean(name="$aop_syslog")
 public class SysLogAopConfigration extends SimpleAopMaker<SLog> {
