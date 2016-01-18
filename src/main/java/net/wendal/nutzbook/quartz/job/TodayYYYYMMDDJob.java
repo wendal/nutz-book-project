bb02edd4ab247cc2d052eb1543bfa55a071fe8b8
wendal <wendal1985@gmail.com>
2016-01-18 19:38:23 +0800

    change: TodayYYYYMMDDJob做成个Bean呗

--- src/main/java/net/wendal/nutzbook/quartz/job/TodayYYYYMMDDJob.java
+++ src/main/java/net/wendal/nutzbook/quartz/job/TodayYYYYMMDDJob.java
@@ -1,11 +1,13 @@
 package net.wendal.nutzbook.quartz.job;
 
+import org.nutz.ioc.loader.annotation.IocBean;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
 import net.wendal.nutzbook.util.Toolkit;
 
+@IocBean
 public class TodayYYYYMMDDJob implements Job {
 
 	public void execute(JobExecutionContext context) throws JobExecutionException {
