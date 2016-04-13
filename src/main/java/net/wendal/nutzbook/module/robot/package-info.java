33101458f1317c6ee3b2cfdb7d451bd0c25e6021
王贵源 <wkipy@hotmail.com>
2016-04-13 11:40:04 +0800

    机器人

---
+++ src/main/java/net/wendal/nutzbook/module/robot/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @author Kerbores(kerbores@gmail.com)
+ *
+ * @project nutzbook
+ *
+ * @file package-info.java
+ *
+ * @description 机器人
+ *
+ * @time 2016年3月8日 上午10:51:26
+ *
+ */
+package net.wendal.nutzbook.module.robot;
