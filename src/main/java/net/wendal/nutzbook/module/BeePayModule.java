7d119105e1d2f1dd11d6583f380501b900e8efc8
wendal chen <wendal1985@gmail.com>
2016-04-18 12:48:45 +0800

    add: 打赏功能,但微信扫描失败,隐藏不显示.

---
+++ src/main/java/net/wendal/nutzbook/module/BeePayModule.java
@@ -0,0 +1,48 @@
+package net.wendal.nutzbook.module;
+
+import org.apache.shiro.authz.annotation.RequiresUser;
+import org.nutz.ioc.impl.PropertiesProxy;
+import org.nutz.ioc.loader.annotation.Inject;
+import org.nutz.ioc.loader.annotation.IocBean;
+import org.nutz.lang.Lang;
+import org.nutz.lang.Strings;
+import org.nutz.lang.random.R;
+import org.nutz.lang.util.NutMap;
+import org.nutz.mvc.annotation.At;
+import org.nutz.mvc.annotation.Ok;
+import org.nutz.mvc.annotation.POST;
+import org.nutz.mvc.annotation.Param;
+
+@IocBean
+@At("/pay/bc")
+public class BeePayModule extends BaseModule {
+    
+    @Inject
+    PropertiesProxy conf;
+
+    @POST
+    @RequiresUser
+    @Ok("json")
+    @At
+    public Object create(@Param("to")int toUserId, @Param("title")String title, @Param("amount")int amount) {
+        String out_trade_no = "bc"+System.currentTimeMillis();
+        if (amount == 0) {
+            amount = R.random(1, 100);
+        }
+        else if (amount < 1)
+            amount = 1;
+        else if (amount > 100)
+            amount = 100;
+        String appid = conf.get("bc.appId");
+        String appSecret = conf.get("bc.appSecret");
+        if (Strings.isBlank(title))
+            title = "test";
+        NutMap re = new NutMap();
+        re.put("out_trade_no", out_trade_no);
+        re.put("amount", amount);
+        re.put("title", title);
+        re.put("sign", Lang.md5(appid+title+amount+out_trade_no+appSecret));
+        re.put("debug", true);
+        return _map("data", re);
+    }
+}
