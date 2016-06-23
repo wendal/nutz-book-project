8603510c52e156b5a2caf83a3f66558302e57229
wendal chen <wendal1985@gmail.com>
2016-06-24 00:07:41 +0800

    update: WebsocketTopic需要同时兼容jetty和tomcat
    Signed-off-by: wendal chen <wendal1985@gmail.com>

--- src/main/java/net/wendal/nutzbook/module/WebsocketTopic.java
+++ src/main/java/net/wendal/nutzbook/module/WebsocketTopic.java
@@ -1,17 +1,17 @@
 package net.wendal.nutzbook.module;
 
-import java.io.IOException;
 import java.util.Map;
 
 import javax.websocket.CloseReason;
 import javax.websocket.Endpoint;
 import javax.websocket.EndpointConfig;
 import javax.websocket.MessageHandler;
-import javax.websocket.RemoteEndpoint;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
-
-import net.wendal.nutzbook.service.yvr.YvrService;
 
 import org.nutz.json.Json;
 import org.nutz.lang.util.NutMap;
@@ -19,51 +19,59 @@ import org.nutz.log.Log;
 import org.nutz.log.Logs;
 import org.nutz.mvc.Mvcs;
 
+import net.wendal.nutzbook.service.yvr.YvrService;
+
 @ServerEndpoint(value = "/yvr/topic/socket")
 public class WebsocketTopic extends Endpoint {
 
-	private final static Log log = Logs.get();
+    private final static Log log = Logs.get();
 
-	@Override
-	public void onClose(Session session, CloseReason closeReason) {
-		log.error("onClose");
-	}
+    @OnClose
+    public void onClose(Session session, CloseReason closeReason) {
+        log.error("onClose");
+    }
 
-	@Override
-	public void onError(Session session, java.lang.Throwable throwable) {
-		log.error("onError");
-	}
+    @OnError
+    public void onError(Session session, java.lang.Throwable throwable) {
+        log.error("onError");
+    }
 
-	@Override
-	public void onOpen(Session session, EndpointConfig config) {
-		RemoteEndpoint.Basic remote = session.getBasicRemote();
-		session.addMessageHandler(new MyMessageHandle(remote));
-	}
+    @OnOpen
+    public void onOpen(Session session, EndpointConfig config) {
+        session.addMessageHandler(new MyMessageHandle(session));
+    }
 
-	private YvrService yvrService;
+    private YvrService yvrService;
 
-	private class MyMessageHandle implements MessageHandler.Whole<String> {
-		private RemoteEndpoint.Basic remote;
+    protected class MyMessageHandle implements MessageHandler.Whole<String> {
 
-		public MyMessageHandle(RemoteEndpoint.Basic remote) {
-			this.remote = remote;
-		}
+        private Session session;
 
-		@Override
-		public void onMessage(String message) {
-			if (yvrService == null)
-				yvrService = Mvcs.ctx().getDefaultIoc().get(YvrService.class);
-			try {
-				NutMap map = Json.fromJson(NutMap.class, message);
-				String topicId = map.getString("id");
-				int replies = map.getInt("replies");
-				Object re = yvrService.check(topicId, replies);
-				if (re instanceof Map) {
-					remote.sendText(Json.toJson(re));
-				}
-			} catch (IOException e) {
-				log.error(e);
-			}
-		}
-	}
+        public MyMessageHandle(Session session) {
+            this.session = session;
+        }
+
+        @Override
+        public void onMessage(String message) {
+            WebsocketTopic.this.onMessage(message, session);
+        }
+    }
+
+    @OnMessage
+    public void onMessage(String message, Session session) {
+        if (yvrService == null)
+            yvrService = Mvcs.ctx().getDefaultIoc().get(YvrService.class);
+        try {
+            NutMap map = Json.fromJson(NutMap.class, message);
+            String topicId = map.getString("id");
+            int replies = map.getInt("replies");
+            Object re = yvrService.check(topicId, replies);
+            if (re instanceof Map) {
+                session.getAsyncRemote().sendText(Json.toJson(re));
+            }
+        }
+        catch (Throwable e) {
+            log.debug("message=" + message, e);
+        }
+    }
 }
