f408da7b90747c00d9ad197c2e17794cb6df1bc0
wendal chen <wendal1985@gmail.com>
2016-07-17 10:59:21 +0800

    add: 添加nutz-web启动器,用于独立启动项目
    Signed-off-by: wendal chen <wendal1985@gmail.com>

---
+++ src/main/java/net/wendal/nutzbook/SelfLauncher.java
@@ -0,0 +1,11 @@
+package net.wendal.nutzbook;
+
+import org.nutz.web.WebLauncher;
+
+public class SelfLauncher extends WebLauncher {
+
+    public static void main(String[] args) {
+        WebLauncher.main(args);
+    }
+
+}
