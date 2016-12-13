56f1e56ce285bb64afb39564a8993e2b813fa665
wendal chen <wendal1985@gmail.com>
2016-12-13 17:28:29 +0800

    ...

--- src/main/java/net/wendal/nutzbook/service/yvr/TopicSearchService.java
+++ src/main/java/net/wendal/nutzbook/service/yvr/TopicSearchService.java
@@ -16,7 +16,6 @@ import org.apache.lucene.index.IndexWriterConfig.OpenMode;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
 import org.apache.lucene.queryparser.classic.ParseException;
-import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
