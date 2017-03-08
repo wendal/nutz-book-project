11a1eb8a95c3a8d1fc6ad0a13c75a5c7bff79500
wendal chen <wendal1985@gmail.com>
2017-03-08 14:20:47 +0800

    update: 改为使用websocket插件

--- nutzcn-core/src/main/java/net/wendal/nutzbook/core/websocket/NutzbookWebsocket.java
+++ nutzcn-core/src/main/java/net/wendal/nutzbook/core/websocket/NutzbookWebsocket.java
@@ -1,11 +1,8 @@
 package net.wendal.nutzbook.core.websocket;
 
-import java.util.Set;
-
-import javax.websocket.EndpointConfig;
-import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
+import org.nutz.integration.jedis.JedisAgent;
 import org.nutz.integration.jedis.pubsub.PubSub;
 import org.nutz.integration.jedis.pubsub.PubSubService;
 import org.nutz.ioc.aop.Aop;
@@ -14,10 +11,13 @@ import org.nutz.ioc.loader.annotation.IocBean;
 import org.nutz.log.Log;
 import org.nutz.log.Logs;
 import org.nutz.plugins.mvc.websocket.AbstractWsEndpoint;
-import org.nutz.plugins.mvc.websocket.WsHandler;
+import org.nutz.plugins.mvc.websocket.NutWsConfigurator;
+import org.nutz.plugins.mvc.websocket.room.JedisRoomProvider;
 
-@ServerEndpoint(value = "/websocket", configurator=NutIocWebSocketConfigurator.class)
-@IocBean(create="init", depose="depose")
+import redis.clients.jedis.Jedis;
+
+@ServerEndpoint(value = "/websocket", configurator=NutWsConfigurator.class)
+@IocBean(create="init")
 public class NutzbookWebsocket extends AbstractWsEndpoint implements PubSub {
 
     protected static final Log log = Logs.get();
@@ -25,32 +25,31 @@ public class NutzbookWebsocket extends AbstractWsEndpoint implements PubSub {
     @Inject
     protected PubSubService pubSubService;
     
-    public static String prefix = "wsroom:";
+    @Inject 
+    protected JedisAgent jedisAgent;
     
     public void init() {
-        pubSubService.reg(prefix+"*", this);
-    }
-    
-    public WsHandler createHandler(Session session, EndpointConfig config) {
-        return new NutzbookWsStringHandler(prefix);
+        roomPrefix = "wsroom:";
+        roomProvider = new JedisRoomProvider(jedisAgent);
+        try (Jedis jedis = jedisAgent.getResource()) {
+            for (String key : jedis.keys(roomPrefix + "*")) {
+                switch(jedis.type(key)) {
+                    case "none":
+                        break;
+                    case "set":
+                        break;
+                    default :
+                        jedis.del(key);
+                };
+            };
+        }
+        pubSubService.reg(roomPrefix+"*", this);
     }
     
     @Aop("redis")
     public void onMessage(String channel, String message) {
-        log.debugf("GET PubSub channel=%s msg=%s", channel, message);
-        Set<String> wsids = roomProvider.wsids(channel);
-        log.debugf("room=%s size=%s", channel, wsids.size());
-        for(String wsid : wsids) {
-            Session session = _sessions.get(wsid);
-            if (session == null)
-                continue;
-            if (!session.isOpen()) {
-                _sessions.remove(wsid);
-                continue;
-            }
-            session.getAsyncRemote().sendText(message);
-        };
+        if (log.isDebugEnabled())
+            log.debugf("GET PubSub channel=%s msg=%s", channel, message);
+        each(channel, (index, session, length) -> session.getAsyncRemote().sendText(message));
     }
-    
-    public void depose(){}
 }
