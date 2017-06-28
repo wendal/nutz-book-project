50c7d2d0bcac02ec94a6fe04e2ca34ae9f956b98
wendal chen <wendal1985@gmail.com>
2017-06-28 12:47:28 +0800

    change: 移除dao.js和ehcache.js, 改成@IocBean工厂方法

---
+++ nutzcn-core/src/main/java/net/wendal/nutzbook/core/ioc/CoreBeanFactory.java
@@ -0,0 +1,60 @@
+package net.wendal.nutzbook.core.ioc;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import javax.sql.DataSource;
+
+import org.nutz.dao.Dao;
+import org.nutz.dao.impl.NutDao;
+import org.nutz.ioc.impl.PropertiesProxy;
+import org.nutz.ioc.loader.annotation.Inject;
+import org.nutz.ioc.loader.annotation.IocBean;
+import org.nutz.lang.Strings;
+import org.nutz.plugins.cache.dao.DaoCacheInterceptor;
+import org.nutz.plugins.cache.dao.impl.provider.EhcacheDaoCacheProvider;
+
+import com.alibaba.druid.pool.DruidDataSource;
+
+import net.sf.ehcache.CacheManager;
+
+@IocBean
+public class CoreBeanFactory {
+
+    @IocBean(name="conf")
+    public PropertiesProxy buildConfigure() {
+        return new PropertiesProxy("custom/");
+    }
+    
+    @IocBean(create="init", depose="close")
+    public DruidDataSource getDataSource(@Inject("refer:conf")PropertiesProxy conf) {
+        return conf.make(DruidDataSource.class, "db.");
+    }
+    
+    @IocBean(name="dao")
+    public Dao getDao(DataSource dataSource, CacheManager cacheManager, PropertiesProxy conf) throws Throwable {
+        NutDao dao = new NutDao(dataSource);
+        // 设置daocache
+        DaoCacheInterceptor daocache = new DaoCacheInterceptor();
+        // TODO 按配置选用不同的cache提供者
+        EhcacheDaoCacheProvider cacheProvider = new EhcacheDaoCacheProvider();
+        cacheProvider.setCacheManager(cacheManager);
+        cacheProvider.init();
+        daocache.setCacheProvider(cacheProvider);
+        String tableNames = conf.get("daocache.tableNames");
+        if (!Strings.isBlank(tableNames)) {
+            daocache.setCachedTableNames(new HashSet<>(Arrays.asList(tableNames.split(","))));
+        }
+        dao.setInterceptors(Arrays.asList(daocache, "log", "time"));
+        return dao;
+    }
+    
+    @IocBean
+    public CacheManager getCacheManager(PropertiesProxy conf) {
+        CacheManager cacheManager = CacheManager.getCacheManager(conf.get("ehcache.name", "nutzbook"));
+        if (cacheManager == null) {
+            cacheManager = CacheManager.getInstance();
+        }
+        return cacheManager;
+    }
+}
