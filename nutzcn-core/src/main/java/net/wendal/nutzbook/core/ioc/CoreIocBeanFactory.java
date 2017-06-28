d0f9720fb49f65413b4f074babdfa3a7b480f052
wendal chen <wendal1985@gmail.com>
2017-06-28 14:01:42 +0800

    change: 改名叫CoreIocBeanFactory,然后加点注释

--- nutzcn-core/src/main/java/net/wendal/nutzbook/core/ioc/CoreIocBeanFactory.java
+++ nutzcn-core/src/main/java/net/wendal/nutzbook/core/ioc/CoreIocBeanFactory.java
@@ -8,7 +8,6 @@ import javax.sql.DataSource;
 import org.nutz.dao.Dao;
 import org.nutz.dao.impl.NutDao;
 import org.nutz.ioc.impl.PropertiesProxy;
-import org.nutz.ioc.loader.annotation.Inject;
 import org.nutz.ioc.loader.annotation.IocBean;
 import org.nutz.lang.Strings;
 import org.nutz.plugins.cache.dao.DaoCacheInterceptor;
@@ -19,18 +18,27 @@ import com.alibaba.druid.pool.DruidDataSource;
 import net.sf.ehcache.CacheManager;
 
 @IocBean
-public class CoreBeanFactory {
+public class CoreIocBeanFactory {
 
+    /**
+     * 生成配置信息
+     */
     @IocBean(name="conf")
     public PropertiesProxy buildConfigure() {
         return new PropertiesProxy("custom/");
     }
     
+    /**
+     * 主数据源
+     */
     @IocBean(create="init", depose="close")
-    public DruidDataSource getDataSource(@Inject("refer:conf")PropertiesProxy conf) {
+    public DruidDataSource getDataSource(PropertiesProxy conf) {
         return conf.make(DruidDataSource.class, "db.");
     }
     
+    /**
+     * 生成主Dao实例
+     */
     @IocBean(name="dao")
     public Dao getDao(DataSource dataSource, CacheManager cacheManager, PropertiesProxy conf) throws Throwable {
         NutDao dao = new NutDao(dataSource);
@@ -49,6 +57,9 @@ public class CoreBeanFactory {
         return dao;
     }
     
+    /**
+     * Ehcache的CacheManager
+     */
     @IocBean
     public CacheManager getCacheManager(PropertiesProxy conf) {
         CacheManager cacheManager = CacheManager.getCacheManager(conf.get("ehcache.name", "nutzbook"));
