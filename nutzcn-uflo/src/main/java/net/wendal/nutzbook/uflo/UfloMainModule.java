371751a3747b26565640428d764ab1c9da4ee2e7
wendal chen <wendal1985@gmail.com>
2017-10-12 14:52:50 +0800

    update: 使用最新的spring插件和uflo 2.0.3,清理不再需要的hack代码

--- nutzcn-uflo/src/main/java/net/wendal/nutzbook/uflo/UfloMainModule.java
+++ nutzcn-uflo/src/main/java/net/wendal/nutzbook/uflo/UfloMainModule.java
@@ -2,7 +2,7 @@ package net.wendal.nutzbook.uflo;
 
 import org.nutz.mvc.annotation.IocBy;
 
-@IocBy(args={"*anno", "net.wendal.nutzbook.uflo", "*spring"})
+@IocBy(args={"*anno", "net.wendal.nutzbook.uflo", "*spring", "classpath*:spring-context.xml"})
 public class UfloMainModule {
 
 }
