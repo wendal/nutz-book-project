9d78420226a05bfbd24df49a9e19ac9e22434159
Wendal Chen <wendal1985@gmail.com>
2018-01-18 12:13:54 +0800

    fix: uflo模块引用的spring版本不能是5.0.0

--- nutzcn-uflo/src/main/java/net/wendal/nutzbook/uflo/UfloMainSetup.java
+++ nutzcn-uflo/src/main/java/net/wendal/nutzbook/uflo/UfloMainSetup.java
@@ -6,6 +6,7 @@ import org.nutz.mvc.Setup;
 public class UfloMainSetup implements Setup {
     
     public void init(NutConfig nc) {
+        nc.getIoc().get(null, "uflo.taskService");
     }
 
     public void destroy(NutConfig nc) {
